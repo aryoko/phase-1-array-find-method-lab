b5eae1d153570a7f471a896a2e51f195196ab7dd
Ary Okoli <aco.umeadi@gmail.com>
2023-04-05 17:41:25 -0400

    phase-1 array find lab partial

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// code your solution here
+const callbackFindWin = function(element, index, record) {
+  return (element.result === 'W') // || record[-1].result === 'N/A')
+}
+
+function superbowlWin(record) {
+  let firstWin
+  firstWin = record.find(callbackFindWin)
+  return (firstWin.result === 'W') ? firstWin.year : firstWin.result
+}
+
+
